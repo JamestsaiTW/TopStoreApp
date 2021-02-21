c0ac364d54ceca2dbfbbabb810924e8de2fbe4d1
James Tsai <jatsai@msn.com>
2021-02-21 19:46:54 +0800

    第 II 回

---
+++ TopStoreApp/TopStoreApp/AppShell.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace TopStoreApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
