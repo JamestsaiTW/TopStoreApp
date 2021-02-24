ea18b69af42511dd0c58df7d153105d2e3bffa14
James Tsai <jatsai@msn.com>
2021-02-24 18:04:20 +0800

    第 V 回

--- TopStoreApp/TopStoreApp/Models/Person.cs
+++ TopStoreApp/TopStoreApp/Models/Person.cs
@@ -2,5 +2,9 @@
 {
     public class Person
     {
+        public string Name { get; set; }
+        public string Tel { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
     }
 }
