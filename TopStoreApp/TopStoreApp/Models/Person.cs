a2dea95bde83866a73223fa3de99e90e75a9c3ca
James Tsai <jatsai@msn.com>
2022-08-23 22:08:00 +0800

    EP 14: The MenuItem of ListView binds Command in itself Model

--- TopStoreApp/TopStoreApp/Models/Person.cs
+++ TopStoreApp/TopStoreApp/Models/Person.cs
@@ -1,4 +1,6 @@
-﻿namespace TopStoreApp.Models
+﻿using Xamarin.Forms;
+
+namespace TopStoreApp.Models
 {
     public class Person : Xam.Plugin.BaseBindingLibrary.BaseNotifyProperty
     {
@@ -37,5 +39,22 @@
             get { return _address; }
             set { OnPropertyChanged<string>(ref _address, value); }
         }
+
+
+        public System.Windows.Input.ICommand DeleteCommand
+        {
+            get
+            {
+                return new Xamarin.Forms.Command(async()=>
+                {
+                    var isOk = await Shell.Current.DisplayAlert("警告", $"確定\"{Name}\"刪除?", "確定", "取消");
+                    if (isOk)
+                    {
+                        Utilities.MockData.People.Remove(this);
+                    }
+                });
+            }
+        }
+
     }
 }
