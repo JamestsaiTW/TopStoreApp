e2b41947b96dfcbfd1d380e171d88b352b0f59da
James Tsai <jatsai@msn.com>
2022-08-23 21:08:15 +0800

    EP 5: Use self-define Code Snippet to improve Coding Experience for NotifyProperty in ViewModel

---
+++ TopStoreApp/TopStoreApp/ViewModels/BasePageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TopStoreApp.ViewModels
+{
+    public class BasePageViewModel : Xam.Plugin.BaseBindingLibrary.BaseNotifyProperty
+    {
+        private bool _isConnected;
+
+        public bool IsConnected
+        {
+            get { return _isConnected; }
+            set { OnPropertyChanged<bool>(ref _isConnected, value); }
+        }
+    }
+}
---
+++ TopStoreApp/TopStoreApp/ViewModels/PeoplePageViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+using TopStoreApp.Models;
+using System.Linq;
+
+namespace TopStoreApp.ViewModels
+{
+    public class PeoplePageViewModel : BasePageViewModel
+    {
+        private ObservableCollection<Person> _people;
+
+        public ObservableCollection<Person> People
+        {
+            get { return _people; }
+            set { OnPropertyChanged<ObservableCollection<Person>>(ref _people, value); }
+        }
+    }
+}
