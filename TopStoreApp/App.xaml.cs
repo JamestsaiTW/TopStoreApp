24bf0d724e7f5c98d6131d9485cb8974ac368d85
James Tsai <jatsai@msn.com>
2022-09-15 01:38:51 +0800

    EP 01: Create A .NET MAUI Project for TopStoreApp

---
+++ TopStoreApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace TopStoreApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ TopStoreApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TopStoreApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
