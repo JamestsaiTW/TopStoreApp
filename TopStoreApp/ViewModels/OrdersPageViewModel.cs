97596bf6c4993e15c8fcc062533d85782bc821e5
James Tsai <jatsai@msn.com>
2023-08-05 23:20:06 +0800

    EP 04

---
+++ TopStoreApp/ViewModels/OrdersPageViewModel.cs
@@ -0,0 +1,11 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace TopStoreApp.ViewModels;
+    
+public partial class OrdersPageViewModel : BasePageViewModel
+{
+    [ObservableProperty]
+    private ObservableCollection<Models.SummaryOrder> _summaryOrders;
+}
+
