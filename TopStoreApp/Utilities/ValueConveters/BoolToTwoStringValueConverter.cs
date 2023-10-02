269173dbf40ea61c0358a7cc4294ec7fe97b1a90
James Tsai <jatsai@msn.com>
2023-10-03 00:52:56 +0800

    EP 17 (A)

---
+++ TopStoreApp/Utilities/ValueConveters/BoolToTwoStringValueConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+
+namespace TopStoreApp.Utilities.ValueConveters;
+
+public class BoolToTwoStringValueConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        var parameterForTrueOrFlase = parameter.ToString().Split(',');
+        return (bool)value ? parameterForTrueOrFlase[0] : parameterForTrueOrFlase[1];
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return string.IsNullOrEmpty((string)value);
+    }
+}
